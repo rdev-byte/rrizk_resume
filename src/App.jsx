cf967e2475b694d4a0aa2acc61f9c2740ee2621a
RDevByte <rdev.byte@gmail.com>
2023-09-27 19:41:30 -0600

    Committing local changes with line ending configuration

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import './App.css';
 import './particle_background/particle.css';
 
 // Import the particlesJS initialization function
-
 import { initializeParticles } from './particle_background/particle';
 
 function App() {
   useEffect(() => {
-    // Initialize particles.js when the component mounts
+    console.log('useEffect is running'); // Check if this message appears in the console
     initializeParticles();
   }, []);
 
@@ -20,6 +19,31 @@ function App() {
       {/* Your existing content */}
       <header className="App-header">
         {/* Your content here */}
+        <nav className="my-16 animate-fade-in">
+          <ul className="flex items-center justify-center gap-4">
+            <a className="text-sm duration-500 text-zinc-500 hover:text-zinc-300" href="/projects">
+              Projects
+            </a>
+            <a className="text-sm duration-500 text-zinc-500 hover:text-zinc-300" href="/contact">
+              Contact
+            </a>
+          </ul>
+        </nav>
+        <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"></div>
+        <div className="absolute inset-0 -z-10 animate-fade-in" aria-hidden="true">
+          <canvas width="1355" height="946" style={{ width: '1355px', height: '946px' }}></canvas>
+        </div>
+        <h1 className="z-10 text-4xl bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+          Raphael Rizk
+        </h1>
+        <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"></div>
+        <div className="my-16 text-center animate-fade-in">
+          <h2 className="text-sm text-zinc-500 ">
+            I'm a software engineer, designer, and family man. I love building things that make people's lives easier.
+          </h2>
+        </div>
+        <canvas width="1355" height="946" style={{ width: '1355px', height: '946px' }}></canvas>
+        <div className="hidden">{/* Add any additional content here */}</div>
       </header>
     </div>
   );
